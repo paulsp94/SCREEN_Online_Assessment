0f4e2358aa90e37a22e2d84aa5108bba88badbf8
Paul Spende <paul.spende@gmail.com>
2019-09-19 17:28:52 -0300

    added Metrics in redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,13 @@ export const setSubType = subType => ({
   type: 'SET_SUBTYPE',
   subType
 });
+
+export const setMetrics = metrics => ({
+  type: 'SET_METRICS',
+  metrics
+});
+
+export const setAdditionalParameters = additionalParameters => ({
+  type: 'SET_ADDITIONAL_PARAMETERS',
+  additionalParameters
+});
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
 import { setProjectProps } from './projectReducer';
+import metricsReducer from './metricsReducer';
 
-export default combineReducers({ setProjectProps });
+export default combineReducers({ setProjectProps, metricsReducer });
