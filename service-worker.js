53b3b900e7bda3faf971761ac93ca36d82a1ac95
Paul Spende <paul.spende@gmail.com>
2019-09-23 12:07:58 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2bb3f8c67a270fe954bc950ce4c44e67.js"
+  "/SCREEN_Online_Assessment/precache-manifest.cf14cb4ec331a876deb4f0b5bd0e1682.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/SCREEN_Online_Assessment/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
