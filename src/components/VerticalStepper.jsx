dcd32b3c127405529e054ba94db9200b5bf091e6
Paul Spende <paul.spende@gmail.com>
2019-11-06 19:06:03 +0100

    component to functional component

--- src/components/VerticalStepper.jsx
+++ src/components/VerticalStepper.jsx
@@ -1,13 +1,14 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import Step from '@material-ui/core/Step';
 import StepContent from '@material-ui/core/StepContent';
 import StepLabel from '@material-ui/core/StepLabel';
 import Stepper from '@material-ui/core/Stepper';
-import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import { calculate } from '../actions';
 import { steps } from '../constants';
 import ClimateStep from './ClimateStep';
 import EmploymentStep from './EmploymentStep';
@@ -31,30 +32,21 @@ const styles = theme => ({
   }
 });
 
-class VerticalLinearStepper extends Component {
-  state = {
-    activeStep: 0
+const VerticalLinearStepper = ({ onFinish, classes }) => {
+  const [activeStep, setActiveStep] = useState(0);
+
+  const handleNext = () => {
+    setActiveStep(activeStep + 1);
+    if (activeStep === steps.length - 1) {
+      onFinish();
+    }
   };
 
-  handleNext = () => {
-    this.setState(state => ({
-      activeStep: state.activeStep + 1
-    }));
-  };
+  const handleBack = () => setActiveStep(activeStep - 1);
 
-  handleBack = () => {
-    this.setState(state => ({
-      activeStep: state.activeStep - 1
-    }));
-  };
+  const handleReset = () => setActiveStep(0);
 
-  handleReset = () => {
-    this.setState({
-      activeStep: 0
-    });
-  };
-
-  getStepContent = step => {
+  const getStepContent = step => {
     switch (step) {
       case 0:
         return <ProjectStep />;
@@ -69,65 +61,87 @@ class VerticalLinearStepper extends Component {
     }
   };
 
-  render() {
-    const { classes } = this.props;
-    const { activeStep } = this.state;
-
-    return (
-      <div
-        className={classes.root}
-        style={{ paddingTop: '10px', margin: 'auto' }}
-      >
-        <Stepper activeStep={activeStep} orientation="vertical">
-          {steps.map((label, index) => (
-            <Step key={label}>
-              <StepLabel>{label}</StepLabel>
-              <StepContent>
-                {this.getStepContent(index)}
-                <div className={classes.actionsContainer}>
-                  <div>
-                    <Button
-                      disabled={activeStep === 0}
-                      onClick={this.handleBack}
-                      type="button"
-                      className={classes.button}
-                    >
-                      Back
-                    </Button>
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      type="submit"
-                      onClick={this.handleNext}
-                      className={classes.button}
-                    >
-                      {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-                    </Button>
-                  </div>
+  return (
+    <div
+      className={classes.root}
+      style={{ paddingTop: '10px', margin: 'auto' }}
+    >
+      <Stepper activeStep={activeStep} orientation="vertical">
+        {steps.map((label, index) => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+            <StepContent>
+              {getStepContent(index)}
+              <div className={classes.actionsContainer}>
+                <div>
+                  <Button
+                    disabled={activeStep === 0}
+                    onClick={handleBack}
+                    type="button"
+                    className={classes.button}
+                  >
+                    Back
+                  </Button>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    type="submit"
+                    onClick={handleNext}
+                    className={classes.button}
+                  >
+                    {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                  </Button>
                 </div>
-              </StepContent>
-            </Step>
-          ))}
-        </Stepper>
-        {activeStep === steps.length && (
-          <Paper square elevation={0} className={classes.resetContainer}>
-            <Typography>All steps completed - you&apos;re finished</Typography>
-            <Button
-              onClick={this.handleReset}
-              type="button"
-              className={classes.button}
-            >
-              Reset
-            </Button>
-          </Paper>
-        )}
-      </div>
-    );
-  }
-}
-
-VerticalLinearStepper.propTypes = {
-  classes: PropTypes.object
+              </div>
+            </StepContent>
+          </Step>
+        ))}
+      </Stepper>
+      {activeStep === steps.length && (
+        <Paper square elevation={0} className={classes.resetContainer}>
+          <Typography>All steps completed - you&apos;re finished</Typography>
+          <Button
+            onClick={handleReset}
+            type="button"
+            onTriggerEducational
+            className={classes.button}
+          >
+            Reset
+          </Button>
+        </Paper>
+      )}
+    </div>
+  );
 };
 
-export default withStyles(styles)(VerticalLinearStepper);
+const mapStateToProps = ({
+  projectReducer,
+  metricReducer,
+  climateReducer,
+  employmentReducer
+}) => ({
+  type: projectReducer.type,
+  subtype: projectReducer.subtype,
+  metric: metricReducer.metric,
+  additionalParameter: metricReducer.additionalParameter,
+  energyBalance: climateReducer.energyBalance,
+  emissionReduction: climateReducer.emissionReduction,
+  employment: employmentReducer.employment
+});
+
+const mapDispatchToProps = dispatch => ({
+  calculate: props => dispatch(calculate(props))
+});
+
+const mergeProps = (state, actions, ownProps) => ({
+  onFinish: () => actions.calculate(state),
+  ...ownProps
+});
+
+export default withStyles(styles)(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps,
+    mergeProps
+  )(VerticalLinearStepper)
+);
