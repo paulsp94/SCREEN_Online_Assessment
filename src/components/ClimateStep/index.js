39bcced0fbf4ed8460c796ea9ef42a95ac17099e
Paul Spende <paul.spende@gmail.com>
2019-12-12 01:55:10 +0100

    refactor ClimateStep

---
+++ src/components/ClimateStep/index.js
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux';
+import { withStyles } from '@material-ui/core/styles';
+
+import { setEnergyBalance, setEmissionReduction } from '../../actions';
+
+import styles from './styles';
+import ClimateStep from './ClimateStep';
+
+const mapStateToProps = ({ climateReducer }) => ({
+  energyBalance: climateReducer.energyBalance,
+  emissionReduction: climateReducer.emissionReduction
+});
+
+const mapDispatchToProps = dispatch => ({
+  onBalanceChange: event => dispatch(setEnergyBalance(event.target.value)),
+  onEmissionChange: event => dispatch(setEmissionReduction(event.target.value))
+});
+
+export default withStyles(styles)(
+  connect(mapStateToProps, mapDispatchToProps)(ClimateStep)
+);
---
+++ src/components/ClimateStep/styles.js
@@ -0,0 +1,18 @@
+const styles = () => ({
+  paper: {
+    marginTop: '10px',
+    width: '40%',
+    padding: '10px',
+    boxSizing: 'border-box',
+    margin: 'auto'
+  },
+  climateDescription: {
+    textAlign: 'left'
+  },
+  climateBody: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  }
+});
+
+export default styles;
